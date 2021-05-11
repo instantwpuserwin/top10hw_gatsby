bec146e80d30b7ebe2dc24be66fc7734adb0a137
Tom George <tomgusa@gmail.com>
2021-05-11 14:51:28 +0530

    1st top10 commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,14 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/pages/404.js"))),
+  "component---src-pages-aboutus-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/pages/aboutus.js"))),
+  "component---src-templates-blog-post-archive-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/templates/blog-post-archive.js"))),
+  "component---src-templates-blog-post-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/templates/blog-post.js")))
+}
+

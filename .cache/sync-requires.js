ad334c4df2147e1c4ffabb65df05f1eb5b65b5f3
Tom George <tomgusa@gmail.com>
2021-05-11 15:44:26 +0530

    test after gabsy build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/pages/404.js"))),
-  "component---src-pages-aboutus-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/pages/aboutus.js"))),
-  "component---src-templates-blog-post-archive-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/templates/blog-post-archive.js"))),
-  "component---src-templates-blog-post-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/templates/blog-post.js")))
+  "component---src-pages-aboutus-js": (preferDefault(require("/home/tgk/code/sites/gatsby/top10hw/src/pages/aboutus.js")))
 }
 

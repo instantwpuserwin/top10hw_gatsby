d6d26ceb620eb23983e15799bd47191fd43bc3f6
Tom George <tomgusa@gmail.com>
2021-05-18 14:54:24 +0530

    instal;ed  gatsby-plugin-netlify-redirect

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,13 +75,8 @@ module.exports = {
 
     // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
     `gatsby-plugin-react-helmet`,
-
-    /**
-     * this (optional) plugin enables Progressive Web App + Offline functionality
-     * To learn more, visit: https://gatsby.dev/offline
-     */
-    // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-meta-redirect`,
+    `gatsby-plugin-netlify-redirect`,
+    `gatsby-plugin-meta-redirect`, 
   ],
 }

38fae030e81804199abb966aa7b36579a3013ed5
Tom George <tomgusa@gmail.com>
2021-05-20 15:27:26 +0530

    added feature image to hp

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,39 +1,43 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import parse from "html-react-parser"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import parse from "html-react-parser";
 
- 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { isEmpty } from 'lodash';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Img from 'gatsby-image';
 
 const BlogIndex = ({
   data,
   pageContext: { nextPagePath, previousPagePath },
 }) => {
-  const posts = data.allWpPost.nodes
+  const posts = data.allWpPost.nodes;
 
   if (!posts.length) {
     return (
       <Layout isHomePage>
         <SEO title="All posts" />
-        
+
         <p>
           No blog posts found. Add posts to your WordPress site and they'll
           appear here!
         </p>
       </Layout>
-    )
+    );
   }
 
   return (
     <Layout isHomePage>
       <SEO title="All posts" />
 
- 
-
       <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.title
+        {posts.map((post) => {
+          const title = post.title;
+
+          const featuredImage = {
+            fluid: post.featuredImage?.node?.localFile?.childImageSharp?.fluid,
+            alt: post.featuredImage?.node?.alt || ``,
+          }
 
           return (
             <li key={post.uri}>
@@ -48,12 +52,22 @@ const BlogIndex = ({
                       <span itemProp="headline">{parse(title)}</span>
                     </Link>
                   </h2>
+
+                  {featuredImage?.fluid && (
+                    <Img
+                      fluid={featuredImage.fluid}
+                      alt={featuredImage.alt}
+                      style={{ marginBottom: 50 }}
+                    />
+                  )}
+          
+
                   <small>{post.date}</small>
                 </header>
                 <section itemProp="description">{parse(post.excerpt)}</section>
               </article>
             </li>
-          )
+          );
         })}
       </ol>
 
@@ -65,10 +79,10 @@ const BlogIndex = ({
       )}
       {nextPagePath && <Link to={nextPagePath}>Next page</Link>}
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query WordPressPostArchive($offset: Int!, $postsPerPage: Int!) {
@@ -83,7 +97,20 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         excerpt
+        featuredImage {
+          node {
+            altText
+            localFile {
+              childImageSharp {
+           
+                fluid(maxWidth: 1000, quality: 100) {
+                  ...GatsbyImageSharpFluid_tracedSVG
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
-`
+`;

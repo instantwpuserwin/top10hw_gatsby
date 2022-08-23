81aabcd5da4692e313034011aca17e30cbef44ac
instantwpuserwin <tomgusa@gmail.com>
2022-08-23 19:03:50 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,7 +27,7 @@ const Footer = props => {
         <div  className="footer_column">
           <h4>About Us</h4>
           <p>
-            At top10hostingonweb.com, we recommend best hosting web hosting companies that suits your business or personal needs. Please note that our reviews are independant. We are also affiliate of some the best web hosting companies and this means we earn a small commission when you sign up. You also get a better price since we offer coupons. 
+            At top10hostingonweb.com, we always recommend best hosting web hosting companies that suits your business or personal needs. Please note that our reviews are independant. We are also affiliate of some the best web hosting companies and this means we earn a small commission when you sign up. You also get a better price since we offer coupons. 
           </p>
         </div>
  
